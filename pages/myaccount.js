5b64051b7aa8b11ec7e2398223c2439f015f829e
Ramazani <ramazanimwemedi234@gmail.com>
2023-05-03 15:09:29 +0300

    Dissabled the swc

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -1,6 +1,12 @@
-import { Box, Typography } from '@mui/material'
-import Head from 'next/head';
-import React from 'react'
+import { Avatar, Box, Typography } from "@mui/material";
+import Head from "next/head";
+import * as React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const MyAccount = () => {
   return (
@@ -12,11 +18,113 @@ const MyAccount = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box>
-        <Typography variant="h1">My Account</Typography>
+      <Box
+        sx={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        {/* Avatar and Username */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{
+              height: 150,
+              width: 150,
+            }}
+            src="https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <Typography variant="h4">Marie Smith</Typography>
+        </Box>
+        {/* Tabs */}
+        <FullWidthTabs />
       </Box>
     </>
   );
-}
+};
 
 export default MyAccount;
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+function FullWidthTabs() {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ bgcolor: "background.paper", width: 500 }}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Box>
+  );
+}
--- pages/trending.js
+++ pages/trending.js
@@ -1,10 +1,14 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 const trending = () => {
   return (
-    <Box>trending</Box>
-  )
-}
+    <Box>
+      <Typography variant="h3" color="green">
+        https://github.com/Codust-SIR/bit{" "}
+      </Typography>
+    </Box>
+  );
+};
 
-export default trending
+export default trending;
