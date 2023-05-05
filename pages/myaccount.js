e0ba90f6491d99f4702932c88ef386b32f9ec0b7
Hiss8n <hissentutu@gmail.com>
2023-05-05 15:56:08 +0300

    I changed left to buttons

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -43,10 +43,16 @@ const MyAccount = () => {
  
       </Box>
       <Box component="form" sx={{p:'3px 5px',display:'grid',left:'20%',width:'100%',backgroundColor:'white'}}>
-        <Typography>Register</Typography>
-        < Typography>Sign Up</Typography>
-        < Typography>Sign Out</Typography>
-        < Typography>Help</Typography>
+       <IconButton>Home</IconButton>
+       <IconButton>personal info</IconButton>
+       <IconButton>data & privacy</IconButton>
+       <IconButton>security</IconButton>
+       <IconButton>people and sharing</IconButton>
+       <IconButton>payments & subscription</IconButton>
+       <hr/>
+       <IconButton>About</IconButton>
+       
+       
       </Box>
     </>
   );
