16ed140906e9372a3a968bf0a53fec56cfe231f2
charzlwebz <charzlwebz256@gmail.com>
2023-05-05 17:22:04 +0300

    Terms and Conditions

--- pages/login.js
+++ pages/login.js
@@ -34,7 +34,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Your Website 
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- pages/termsandpolicy.js
+++ pages/termsandpolicy.js
@@ -1,9 +1,41 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Typography } from "@mui/material";
+
 
 const termsandpolicy = () => {
   return (
-    <Box>termsandpolicy</Box>
+    <Box>
+<Typography variant="h3" color="text.secondary" align="center">AT BIT, WE TAKE PRIVACY VERY SERIOUS, HERE ARE OUR TERMS AND CONDITIONS GOVERNING OUR SERVICES</Typography>
+<Typography variant="h4" color="text.secondary"> Terms and Conditions:</Typography>
+
+<Box>
+<Typography variant="h6" color="text.secondary">
+    <ol>
+    <li>Acceptance of Terms and Conditions: These terms and conditions constitute a legal agreement between you and BIT regarding your use of our services in connection with the design and development of a YouTube clone. By using our services, you agree to be bound by these Terms.</li>
+        <br/>
+    <li>Ownership of Intellectual Property: You acknowledge that we own all right, title, and interest in and to the Project, including all intellectual property rights therein. You agree not to use or disclose any part of the Project except as expressly permitted under these Terms or with our prior written consent</li>
+    <br/>
+    <li>Confidentiality: You agree to keep confidential and not to disclose any confidential information or trade secrets of ours, including without limitation, the design of the Project, source code, documentation, and any other information or materials that are marked or otherwise identified as confidential ("Confidential Information"). You agree to use the Confidential Information solely for the purposes of the Project and not for any other purpose without our prior written consent</li>
+    <br/>
+    <li>Payment: You agree to pay us the agreed-upon fee for the design and development of the Project. The payment terms will be set forth in a separate agreement between you and us</li>
+    <br/>
+    <li>Warranties: You represent and warrant that you have the legal right and authority to enter into these Terms and to perform your obligations under these Terms. You further represent and warrant that the Project does not infringe upon the intellectual property rights of any third party</li>
+    <br/>
+    <li>Limitation of Liability: You acknowledge that we will not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or relating to these Terms or the Project, whether or not we have been advised of the possibility of such damages.</li>
+    <br/>
+    <li>Indemnification: You agree to indemnify and hold us harmless from and against any and all claims, damages, liabilities, costs, and expenses (including reasonable attorneys' fees) arising out of or relating to your breach of these Terms or your use of the Project</li>
+    <br/>
+    <li>Termination: These Terms will remain in effect until the completion of the Project or until terminated by either party upon written notice to the other. Upon termination, you will immediately cease all use of the Project and return all Confidential Information to us</li>
+    <br/>
+    <li>Governing Law: These Terms will be governed by and construed in accordance with the laws of Kenya without giving effect to any choice or conflict of law provision or rule.</li>
+    <br/>
+    <li>Entire Agreement: These Terms constitute the entire agreement between you and us with respect to the Project and supersede all prior or contemporaneous communications and proposals, whether oral or written, between you and us</li>
+</ol>
+</Typography>
+<Typography variant="h4" color="text.secondary">Cookies</Typography>
+</Box>
+    </Box>
   )
 }
 
