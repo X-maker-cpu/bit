bc8673e5654a3dd007cdd434c3f833b4d01a58cb
Hiss8n <hissentutu@gmail.com>
2023-05-20 10:31:31 +0300

    Fixed

---
+++ pages/categories/[id]/index.js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const Index = () => {
+    const router =useRouter();
+    return(
+        <div>
+            <iframe
+            width="1280"
+            height="720"
+            src={`https://www.youtube.com/embed/${router.query.id}`}
+            frameborder="0"
+            allowFullScreen
+            ></iframe>
+        </div>
+    );
+};
+
+export default Index;
